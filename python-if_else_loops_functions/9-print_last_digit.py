05aa6db60b3d1451435e55488d22d644619d0a53
Ayxan <ayxanps42007@gmail.com>
2025-11-15 13:10:52 +0000

    enough

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
    last = abs(number) % 10
-   print(last, end="")
-    
-print_last_digit(6)    
-print_last_digit(5698)    
-print_last_digit(-3456)
+   return last
