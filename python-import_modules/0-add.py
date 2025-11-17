6e70bcb73505df0275ce9041add676bbd65cc2c1
Ayxan <ayxanps42007@gmail.com>
2025-11-17 08:29:28 +0000

    start

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 # Import the specific function 'add' from the module 'add_0'
 from add_0 import add
-
-
-a = 1
-b = 2
+if __name__ == "__main__":
+    a = 1
+    b = 2
 
 
 print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
