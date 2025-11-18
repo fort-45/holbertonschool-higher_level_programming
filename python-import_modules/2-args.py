0244ebc4ca6e8e3242739e7b95471958f03c16e9
Ayxan <ayxanps42007@gmail.com>
2025-11-18 09:00:29 +0000

    here we are now

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,11 +6,10 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif length == 2:
         print("1 argument:")
-        print(f"1: {sys.argv[1]}")
-
+        print(f"1: {sys.argv[1]}"i)
     else:
         print(f"{length - 1} arguments:")
-        c=1
-        for i in range (1, length):
+        c = 1
+        for i in range(1, length):
             print(f"{c}: {sys.argv[i]}")
-            c+=1
+            c += 1
