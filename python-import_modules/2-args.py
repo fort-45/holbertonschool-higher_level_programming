f38af2822ee4f6c97d33ea74d458fbd799b5c0b9
Ayxan <ayxanps42007@gmail.com>
2025-11-18 08:57:21 +0000

    does not even matter

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    length = len(sys.argv)
+    if length == 1:
+        print("0 arguments.")
+    elif length == 2:
+        print("1 argument:")
+        print(f"1: {sys.argv[1]}")
+
+    else:
+        print(f"{length - 1} arguments:")
+        c=1
+        for i in range (1, length):
+            print(f"{c}: {sys.argv[i]}")
+            c+=1
