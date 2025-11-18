2dc04dccd710ebe8c97230b2b4ea9be849ab4a0c
Ayxan <ayxanps42007@gmail.com>
2025-11-18 09:19:37 +0000

    forever

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 import sys
+
+
 def infinite_add():
     total_sum = 0
-    # sys.argv[1:] creates a slice that includes only the arguments, 
-    # skipping the script name (sys.argv[0]).
     arguments = sys.argv[1:]
-    
     # Iterate through the argument strings
     for arg_string in arguments:
         # Cast the string argument to an integer and add it to the total sum
@@ -13,12 +12,11 @@ def infinite_add():
             number = int(arg_string)
             total_sum += number
         except ValueError:
-            # According to the prompt, we can assume all arguments are integers,
-            # but this structure is good practice for safety.
             pass
 
     # Print the final result followed by a new line
     print(total_sum)
+
 
 if __name__ == "__main__":
     infinite_add()
