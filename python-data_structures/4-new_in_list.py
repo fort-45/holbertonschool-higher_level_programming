8d16a7e418ec02861f2ebd601d904e232fb58a6a
Ayxan <ayxanps42007@gmail.com>
2025-11-21 06:38:04 +0000

    high stakes

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -7,5 +7,6 @@ def new_in_list(my_list, idx, element):
     elif idx >= len(my_list):
         return my_list
     else:
-        my_list[idx] = element
-        
+        new_list = my_list[:]
+        new_list[idx] = element
+        return new_list 
