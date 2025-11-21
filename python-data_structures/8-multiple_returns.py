91f24f3d78ac3f0113162d489e30ee6515edbc2c
Ayxan <ayxanps42007@gmail.com>
2025-11-21 08:47:37 +0000

    higher definiton of human being

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    if sentence == "":
+        first_char = None
+    else:
+        first_char = sentence[0]
+    length = len(sentence)
+    return length, first_char
