c14752ac9494cfe0ef214b2b5e01079581718876
Ayxan <ayxanps42007@gmail.com>
2025-11-22 18:21:54 +0000

    these all will be named same

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -5,6 +5,6 @@ def search_replace(my_list, search, replace):
     for char in my_list:
         new_list = []
         if char == search:
-            new_list,append(replace)
+            new_list.append(replace)
         else:
             new_list.append(char)
