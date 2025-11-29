da823b45762d657e03f757fdf1e78ad81ce18526
Ayxan <ayxanps42007@gmail.com>
2025-11-29 10:05:51 +0000

    same name for every commit

---
+++ python-more_classes/7-rectangle.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+'''
+ha ha ha document
+'''
+
+
+class Rectangle:
+    '''
+    whooooo houuuu
+    '''
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        '''
+        Initializes a new Rectangle instance
+        '''
+        Rectangle.number_of_instances += 1
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        '''
+        salam salam
+        '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''
+        setter fir width
+        '''
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        '''
+        salam salam
+        '''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''
+        setter for height
+        '''
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        rect = ""
+        for _ in range(self.height):
+            rect += self.print_symbol * self.width + "\n"
+        return rect.rstrip()
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
