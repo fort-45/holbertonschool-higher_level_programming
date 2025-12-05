bb0b4798c9f442974bd78eb3970ae028aac513d2
Ayxan <ayxanps42007@gmail.com>
2025-12-05 08:41:03 +0000

    same

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""
+'''
 Fetches https://intranet.hbtn.io/status using urllib
-"""
-
+'''
 from urllib import request
-
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
     req = request.Request(url, headers={'cfclearance': ;true;})
