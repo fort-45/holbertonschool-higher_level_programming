2653540a532de8978e969e932f418477f55de299
Ayxan <ayxanps42007@gmail.com>
2025-12-05 08:40:27 +0000

    same

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-'''
-fetch the link
-'''
+"""
+Fetches https://intranet.hbtn.io/status using urllib
+"""
+
 from urllib import request
 
 if __name__ == "__main__":
-    url = https://intranet.hbtn.io/status
+    url = "https://intranet.hbtn.io/status"
+    req = request.Request(url, headers={'cfclearance': ;true;})
+    with request.urlopen(url) as response:
+        body = response.read()
+
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
